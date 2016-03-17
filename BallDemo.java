4d6b49ab82a9c97277a741472a0c9e47097ec092
FranFuentesA <fran_fuentes20@hotmail.com>
2016-03-17 12:05:07 +0100

    Podremos introducir el numero de bolas que queresmos crear y se mueven

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -23,7 +24,7 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numeroBolas)
     {
         int ground = 400;   // position of the ground line
 
@@ -31,23 +32,35 @@ public class BallDemo
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
+        int countX = 50;
+        int countY = 60;
+        
+        ArrayList<BouncingBall> bolas = new ArrayList<BouncingBall>(); //Array donde guardaremos las bolas
+        for(int i = 0; i < numeroBolas; i++) {
+            // crate and show the balls
+            BouncingBall ball = new BouncingBall(countX, countY, 16, Color.BLUE, ground, myCanvas);
+            ball.draw();  
+            bolas.add(ball);
+            countX+=32;
+            countY+=45;
+        }
         // make them bounce
         boolean finished =  false;
-        while(!finished) {
+        
+         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for(int i = 0;i < bolas.size();i++)
+            {
+                bolas.get(i).move();
+                // stop once ball has travelled a certain distance on x axis
+                if (bolas.get(i).getXPosition() >= 550)
+                {
+                    finished = true;
+                }
             }
         }
     }
-}
+
+
+    }
+
